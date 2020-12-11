a014189d1062d770235c25b7b0a87ee360f581d6
panka <pankajshinde798@gmail.com>
2020-12-11 19:28:32 +0530

    second

--- Profile/permissions.py
+++ Profile/permissions.py
@@ -5,4 +5,8 @@ class UserOwnObjectPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        else:
+            if request.method == 'GET':
+                return True
+
         return obj.id == request.user.id
--- Profile/views.py
+++ Profile/views.py
@@ -6,9 +6,28 @@ from rest_framework import status, viewsets
 from .models import UserProfile
 from rest_framework.authentication import TokenAuthentication
 from .permissions import UserOwnObjectPermission
+from rest_framework.filters import SearchFilter
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.models import Token
 
 
-# Create your views here.
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
 
 
 
@@ -23,3 +42,5 @@ class UserProfileView(viewsets.ModelViewSet):
         TokenAuthentication,
 
     ]
+    filter_backends = (SearchFilter,)
+    search_fields=('name','email',)
